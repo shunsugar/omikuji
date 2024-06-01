8a969fb829520888472232ce6002038d89cc3c4f
shunsugar <shun.saito.6t@stu.hosei.ac.jp>
2024-06-01 16:44:52 +0900

    reposition text label

--- scripts/omikuji.py
+++ scripts/omikuji.py
@@ -7,7 +7,8 @@ def ResetWindow():
     canvas.delete("all")
 
 def DivineFortune():
-    canvas.create_text(200, 200, text="大吉", font=("Meiryo", 50))
+    canvas.create_text(220, 220, text="大吉", font=("Meiryo", 50))
+    canvas.create_text(350, 290, text="何事もうまくいくよ！", font=("Meiryo", 12))
 
 # Create root window
 root = Tk()
@@ -26,7 +27,7 @@ title_label = Label(root, text="おみくじ (Omikuji)", font=("Meiryo", 20))
 introduction_ja_label = Label(root, text="あなたの運勢は・・・", font=("Meiryo", 12))
 introduction_en_label = Label(root, text="Your fortune is ...", font=("Meiryo", 12))
 #center_label = Label(root, text="大吉", font=("Meiryo", 50))
-brief_comment_label = Label(root, text="何事もうまくいくよ！", font=("Meiryo", 12))
+#brief_comment_label = Label(root, text="何事もうまくいくよ！", font=("Meiryo", 12))
 attention_label = Label(root, text="*This omikuji is not true.", font=("Meiryo", 8))
 
 start_button = Button(root, text="占う (Start)", font=("Meiryo", 14), command=DivineFortune)
@@ -41,7 +42,7 @@ title_label.place(x=100, y=10)
 introduction_ja_label.place(x=40, y=100)
 introduction_en_label.place(x=40, y=130)
 #center_label.place(x=150, y=170)
-brief_comment_label.place(x=250, y=290)
+#brief_comment_label.place(x=250, y=290)
 
 start_button.place(x=160, y=330)
 reset_button.place(x=100, y=390)
