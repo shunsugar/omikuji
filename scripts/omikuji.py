ebcd1e06c876903cc047b0a452092564c5860510
shunsugar <shun.saito.6t@stu.hosei.ac.jp>
2024-08-06 18:19:44 +0900

    The omikuji is completed.

--- scripts/omikuji.py
+++ scripts/omikuji.py
@@ -1,14 +1,36 @@
 from tkinter import *
+import random
 
 def KillWindow():
     root.destroy()
 
 def ResetWindow():
     canvas.delete("all")
+    if start_button["state"] == DISABLED:
+        start_button["state"] = NORMAL
 
 def DivineFortune():
-    canvas.create_text(220, 220, text="大吉", font=("Meiryo", 50))
-    canvas.create_text(350, 290, text="何事もうまくいくよ！", font=("Meiryo", 12))
+    i = random.randrange(1, 100, 1)
+    if (0 < i <= 10):
+        canvas.create_text(220, 220, text="凶", font=("Meiryo", 50))
+        canvas.create_text(320, 290, text="いっぱい食べていっぱい寝てね！", font=("Meiryo", 12))
+    if (10 < i <= 25):
+        canvas.create_text(220, 220, text="末吉", font=("Meiryo", 50))
+        canvas.create_text(340, 290, text="ちょっとひと休みしよう！", font=("Meiryo", 12))
+    if (25 < i <= 50):
+        canvas.create_text(220, 220, text="小吉", font=("Meiryo", 50))
+        canvas.create_text(350, 290, text="散歩に出かけよう！", font=("Meiryo", 12))
+    if (50 < i <= 75):
+        canvas.create_text(220, 220, text="中吉", font=("Meiryo", 50))
+        canvas.create_text(350, 290, text="慌てずに進めよう！", font=("Meiryo", 12))
+    if (75 < i <= 90):
+        canvas.create_text(220, 220, text="吉", font=("Meiryo", 50))
+        canvas.create_text(350, 290, text="何事もうまくいくよ！", font=("Meiryo", 12))
+    if (90 < i <= 100):
+        canvas.create_text(220, 220, text="大吉", font=("Meiryo", 50))
+        canvas.create_text(350, 290, text="今日は絶好調だね！", font=("Meiryo", 12))
+    if start_button["state"] == NORMAL:
+        start_button["state"] = DISABLED
 
 # Create root window
 root = Tk()
@@ -22,32 +44,20 @@ canvas.place(x=0, y=0)
 
 # Define objects
 title_label = Label(root, text="おみくじ (Omikuji)", font=("Meiryo", 20))
-#detail_label = Label(root, text="今日の運勢を占ってみよう！ (Let's read today's fortune!)", font=("Meiryo", 12))
-
 introduction_ja_label = Label(root, text="あなたの運勢は・・・", font=("Meiryo", 12))
 introduction_en_label = Label(root, text="Your fortune is ...", font=("Meiryo", 12))
-#center_label = Label(root, text="大吉", font=("Meiryo", 50))
-#brief_comment_label = Label(root, text="何事もうまくいくよ！", font=("Meiryo", 12))
-attention_label = Label(root, text="*This omikuji is not true.", font=("Meiryo", 8))
-
 start_button = Button(root, text="占う (Start)", font=("Meiryo", 14), command=DivineFortune)
-stop_button = Button(root, text="")
-reset_button = Button(root, text="もう一度 (Retry)", font=("Meiryo", 8), command=ResetWindow)
+reset_button = Button(root, text="もう一度 (Reset)", font=("Meiryo", 8), command=ResetWindow)
 kill_button = Button(root, text="閉じる (Exit)", font=("Meiryo", 8), command=KillWindow)
+attention_label = Label(root, text="*This omikuji is not true.", font=("Meiryo", 8))
 
 # Layout objects
 title_label.place(x=100, y=10)
-#detail_label.place(x=10, y=60)
-
 introduction_ja_label.place(x=40, y=100)
 introduction_en_label.place(x=40, y=130)
-#center_label.place(x=150, y=170)
-#brief_comment_label.place(x=250, y=290)
-
 start_button.place(x=160, y=330)
 reset_button.place(x=100, y=390)
 kill_button.place(x=250, y=390)
-
 attention_label.place(x=300, y=430)
 
 # Display window
